660d217a2cbb0d294d17af7d0748b08d424254ea
Onsol <onsoltu@gmail.com>
2021-10-12 16:41:35 +0300

    ∙ исправлена ошибка при инициализации

--- LoadFile/LoadSQL.cs
+++ LoadFile/LoadSQL.cs
@@ -6,7 +6,18 @@ namespace XMLReading_WPF
     {
         public void Load(Dictionary<string, string> args, out LoadObj fsSource)
         {
-            fsSource = new LoadObj_SQL();
+            string connectionString;
+            if (args.ContainsKey("ServerSettings") == true)
+            {
+                string[] settings = args["ServerSettings"].Split(';');
+                connectionString = @"Data Source=" + settings[0] + ";Initial Catalog=" + settings[1] + ";User ID=" + settings[2] + ";Password=" + settings[3];
+            }
+            else
+            {
+                connectionString = "";
+            }
+
+            fsSource = new LoadObj_SQL(connectionString);
         }
 
         public string Variant() => "с сервера SQL";
